33e6a377012028f7ff5183338ef0a7ccd743fad7
blackweeed <blackweeeed@gmail.com>
2023-01-14 14:58:06 +0100

    getting an error

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import Navbar from "./components/Navbar";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
     </>
   );
 }
